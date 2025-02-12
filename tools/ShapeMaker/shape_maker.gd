58a170b6f8ef91e97e52cacbc737f790f04bbfe8
Kyle S <koocooplatter@users.noreply.github.com>
2025-02-12 18:23:33 -0500

    Structure and tool foundation

---
+++ tools/ShapeMaker/shape_maker.gd
@@ -0,0 +1,9 @@
+@tool #This annotation indicates the script runs in the editor.
+extends Node2D
+
+# -- Enums --
+# Define the shape types as an enum for user-friendly selection in the Inspector.
+enum ShapeType {
+	CIRCLE,
+	RECTANGLE
+}
