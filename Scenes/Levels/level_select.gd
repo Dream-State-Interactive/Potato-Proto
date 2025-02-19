1e8fcf221e165318ac6f17b12e3385cd4d5ae8ac
Alex Lagas <alagas5996@gmail.com>
2025-02-19 18:57:41 -0500

    Added basic main menu

---
+++ Scenes/Levels/level_select.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_level_1_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Levels/proto/level_1_proto.tscn")
+
+
+func _on_level_2_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_level_3_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_level_4_button_pressed() -> void:
+	pass # Replace with function body.
