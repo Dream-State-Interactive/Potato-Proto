40fdcc1e920409139810f9d2a182ef4b05fa475f
Alex Lagas <alagas5996@gmail.com>
2025-02-19 20:53:01 -0500

    Reworked manu changing

--- Scenes/Levels/Menus/main_menu.gd
+++ Scenes/Levels/Menus/main_menu.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var level_select = preload("res://Scenes/Levels/Menus/level_select.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,4 +11,5 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_level_select_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Levels/Menus/level_select.tscn")
+	self.remove_child($Menu)
+	self.add_child(level_select.instantiate())
