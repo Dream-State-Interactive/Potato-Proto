1c868a136b4c0a58019f5c5b90b975b8f1191061
Alex Lagas <alagas5996@gmail.com>
2025-04-09 21:57:44 -0400

    More settings work

--- assets/GUI/MENU/slider.gd
+++ assets/GUI/MENU/slider.gd
@@ -5,13 +5,10 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	masterVolumeSlider.set_value_no_signal(SettingsService.get("master_volume"))
+	masterVolumeSlider.set_value_no_signal(SettingsService.getSettingValue("audio", "master_volume"))
 
 func _on_h_slider_drag_ended(value_changed: bool) -> void:
 	if(value_changed):
 		var new_master_val = masterVolumeSlider.get_value()
-		SettingsService._set("master_volume", masterVolumeSlider.get_value())
+		SettingsService.setSettingValue("audio", "master_volume", masterVolumeSlider.get_value())
 		AudioService.set_master_volume(new_master_val)
-
-func _set(setting: StringName, value: Variant) -> bool:
-	return SettingsService._set(setting, value)
