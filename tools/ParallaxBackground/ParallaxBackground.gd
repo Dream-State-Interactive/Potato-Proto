e366d9a7b1f4dd74b8cabe0830d022d55ac217a5
Kyle S <koocooplatter@users.noreply.github.com>
2025-07-21 22:59:28 -0400

    Fix menu button

--- tools/ParallaxBackground/ParallaxBackground.gd
+++ tools/ParallaxBackground/ParallaxBackground.gd
@@ -8,11 +8,12 @@ func _ready():
 	camera_node = get_viewport().get_camera_2d()
 
 func _process(_delta):
-	if camera_node:
-		# Compute an almost imperceptible scale effect
-		var effective_zoom = Vector2.ONE + (camera_node.zoom - Vector2.ONE) * zoom_dampening
-		
-		# Add subtle scaling ONLY to parallax layers
-		for childLayer in get_children():
-			if childLayer is ParallaxLayer:
-				childLayer.scale = childLayer.scale.lerp(effective_zoom, 0.05)  # Super smooth transition
+	if not is_instance_valid(camera_node):
+		return
+	# Compute an almost imperceptible scale effect
+	var effective_zoom = Vector2.ONE + (camera_node.zoom - Vector2.ONE) * zoom_dampening
+	
+	# Add subtle scaling ONLY to parallax layers
+	for childLayer in get_children():
+		if childLayer is ParallaxLayer:
+			childLayer.scale = childLayer.scale.lerp(effective_zoom, 0.05)  # Super smooth transition
