59f62a4852cc9cccf4d404d1c7c6d73cd941349d
Alex Lagas <alagas5996@gmail.com>
2025-09-07 17:08:30 -0400

    Update how dropdown menu is populated when initialized

--- src/ui/components/dropdown_selection.gd
+++ src/ui/components/dropdown_selection.gd
@@ -2,6 +2,8 @@ extends OptionButton
 class_name DropdownSelection
 
 @onready var LINKED_SETTING = get_meta("linkedSettingName")
+var section
+var key
 
 @export var linkedSettingValues: Dictionary:
 	set(value):
@@ -12,10 +14,16 @@ class_name DropdownSelection
 			i += 1
 	get():
 		return linkedSettingValues
-
-func _on_item_selected(index: int) -> void:
+		
+func _ready() -> void:
 	if(LINKED_SETTING):
 		var settingArray = LINKED_SETTING.split('.')
-		var section = settingArray[0]
-		var key = settingArray[1]
+		section = settingArray[0]
+		key = settingArray[1]
+		var initialTextValue = linkedSettingValues.find_key(SettingsService.getSettingValue(section, key))
+		text = initialTextValue
+		selected = linkedSettingValues.keys().find(initialTextValue)
+
+func _on_item_selected(index: int) -> void:
+	if(section && key):
 		SettingsService.setSettingValue(section, key, linkedSettingValues[linkedSettingValues.keys()[index]])
