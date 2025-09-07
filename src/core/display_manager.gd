365910f89146b846b203a3d3221a39e80fa214ed
Alex Lagas <alagas5996@gmail.com>
2025-09-07 17:15:24 -0400

    Misc fixes

--- src/core/display_manager.gd
+++ src/core/display_manager.gd
@@ -1,28 +1,28 @@
 extends Node
 
-var WINDOW
-var VIEWPORT
+@onready var WINDOW = get_window()
+@onready var VIEWPORT = get_viewport()
 
 @export var display_mode: DisplayServer.WindowMode:
 	set(mode):
 		DisplayServer.window_set_mode(mode, 0)
-		# This doesn't work rn
+		
 		if(mode != DisplayServer.WindowMode.WINDOW_MODE_FULLSCREEN):
-			DisplayServer.window_set_flag(DisplayServer.WindowFlags.WINDOW_FLAG_BORDERLESS, true, WINDOW.get_window_id())
+			DisplayServer.window_set_flag(DisplayServer.WindowFlags.WINDOW_FLAG_BORDERLESS, false, WINDOW.get_window_id())
 	get:
 		return DisplayServer.window_get_mode(0)
 		
 @export var display_resolution: Vector2i:
 	set(display_resolution):
-		VIEWPORT.size = display_resolution
+		WINDOW.size = display_resolution
 	get():
-		return VIEWPORT.size
+		return WINDOW.size
 
 func _ready() -> void:
-	WINDOW = get_window()
-	VIEWPORT = get_viewport()
 	if(WINDOW == null):
 		print("Error: Window not found")
+	if(VIEWPORT == null):
+		print("Error: Viewport not found")
 	
 	display_mode = SettingsService.getSettingValue("display", "display_mode")
 	display_resolution = SettingsService.getSettingValue("display", "display_resolution")
