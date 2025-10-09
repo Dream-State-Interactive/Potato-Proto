0101e08a266169b2b6367e3ff8d706a20cab8f2e
Alex Lagas <alagas5996@gmail.com>
2025-10-08 21:25:39 -0400

    Bug fix

--- tools/ParallaxBackground/fullscreen_control_clouds.gd
+++ tools/ParallaxBackground/fullscreen_control_clouds.gd
@@ -2,7 +2,7 @@ extends Control
 
 func _ready():
 	# Set the control's size to a fixed large area (larger than viewport).
-	size = Vector2(2048, 2048)
+	set_deferred("size", Vector2(2048, 2048))
 	
 	# Position the control so it's centered within the viewport.
 	var vp_size = get_viewport().get_visible_rect().size
