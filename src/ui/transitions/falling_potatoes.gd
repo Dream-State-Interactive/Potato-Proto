832bd72be7dc5f982e25d1c203da7d8a1d2b533b
Alex Lagas <alagas5996@gmail.com>
2025-11-26 17:37:26 -0500

    Tuned the transition to feel better

--- src/ui/transitions/falling_potatoes.gd
+++ src/ui/transitions/falling_potatoes.gd
@@ -2,8 +2,10 @@ extends Node2D
 
 @onready var black_rect: ColorRect = $BlackBackground
 @onready var particles: GPUParticles2D = $Potato_Wipe
+@onready var hold_black_screen_timer: Timer = $HoldBlackScreenTimer
 
-const fade_time = 2.0
+const fade_in_time = 1.25
+const fade_out_time = 0.85
 
 signal transition_opaque
 signal transition_completed
@@ -19,16 +21,20 @@ func start_transition() -> void:
 
 	# Fade to black while particles are falling
 	var tween_in = create_tween()
-	tween_in.tween_property(black_rect, "modulate:a", 1.0, fade_time)
+	tween_in.tween_property(black_rect, "modulate:a", 1.0, fade_in_time)
 	await tween_in.finished
+
+	hold_black_screen_timer.start()
+
+func _on_hold_black_screen_timer_timeout() -> void:
 	transition_opaque.emit()
-	
+
 func finish_transition() -> void:
 
 	# 4) Start fading back to transparent, stop emitting new particles
 	particles.emitting = false
 
 	var tween_out = create_tween()
-	tween_out.tween_property(black_rect, "modulate:a", 0.0, fade_time)
+	tween_out.tween_property(black_rect, "modulate:a", 0.0, fade_out_time)
 	await tween_out.finished
 	queue_free()
