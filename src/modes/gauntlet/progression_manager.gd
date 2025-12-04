23b40dd377471a3925b69a7ca1465592632d1632
Alex Lagas <alagas5996@gmail.com>
2025-12-03 20:42:31 -0500

    Clearing debugger stuff

--- src/modes/gauntlet/progression_manager.gd
+++ src/modes/gauntlet/progression_manager.gd
@@ -17,6 +17,7 @@ func update_progress(new_index: int):
 	if new_index > max_forward_index:
 		max_forward_index = new_index
 		# The global current_level is still useful for tracking the player's best run
+		@warning_ignore("integer_division")
 		var new_level: int = (max_forward_index / 2) + 1
 		if new_level != current_level:
 			current_level = new_level
