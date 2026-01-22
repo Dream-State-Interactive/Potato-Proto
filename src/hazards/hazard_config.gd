4a2fad6af7562cdd917daf34a2b6af5ca10db272
Kyle S <koocooplatter@users.noreply.github.com>
2026-01-21 22:02:13 -0500

    Integrate hazard generation with new level generating system

--- src/hazards/hazard_config.gd
+++ src/hazards/hazard_config.gd
@@ -1,4 +1,4 @@
-@tool
+# src/tools/LevelGen/core/hazard_config.gd
 class_name HazardConfig
 extends Resource
 
@@ -6,26 +6,26 @@ extends Resource
 @export var hazard_scene: PackedScene
 
 @export_group("Spawn Conditions")
-## The first hill number this hazard can appear on (inclusive).
-@export var min_hills_completed: int = 0
-## The last hill this hazard can appear on. Use a high number for 'forever'.
-@export var max_hills_completed: int = 1000
+## The first chunk index this hazard can appear on (inclusive).
+@export var min_chunk_index: int = 0
+## The last chunk index this hazard can appear on. Use a high number for 'forever'.
+@export var max_chunk_index: int = 10000
 
 @export_group("Spawn Behavior & Scaling")
 ## The base probability (0.0 to 1.0) of spawning when it first appears.
 @export_range(0.0, 1.0) var base_density: float = 0.05
 
-## How much to add to the density for each hill completed *after* min_hills_completed.
+## How much to add to the density for each chunk completed *after* min_chunk_index.
 ## Set to 0 for a fixed density.
-@export var density_increase_per_hill: float = 0.001
+@export var density_increase_per_chunk: float = 0.001
 
 ## The absolute maximum density this hazard can reach.
 @export_range(0.0, 1.0) var max_density: float = 0.25
 
-## At what percentage of the hill's length (0.0 to 1.0) should we start boosting the spawn rate.
+## At what percentage of the chunk's length (0.0 to 1.0) should we start boosting the spawn rate.
 @export_range(0.0, 1.0) var end_boost_start_percent: float = 0.8
 
-## How much to multiply the density by in the final section of the hill.
+## How much to multiply the density by in the final section of the chunk.
 @export var end_boost_multiplier: float = 1.0
 
 ## The minimum scale for the spawned hazard. (1, 1) is default size.
@@ -34,5 +34,5 @@ extends Resource
 ## The maximum scale for the spawned hazard. (1, 1) is default size.
 @export var max_scale: Vector2 = Vector2.ONE
 
-## How many placement "slots" this hazard occupies on the hill surface.
-@export_range(1, 10) var slot_cost: int = 1
+## How many placement "slots" (terrain vertices) this hazard occupies (within current system vertices are spaced around 20px).
+@export_range(1, 10000) var slot_cost: int = 3
